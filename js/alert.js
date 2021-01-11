bbe2d2b88064c6b068552465f089d3d519d161ad
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2021-01-11 19:29:02 +0100

    collage claudia

--- js/alert.js
+++ js/alert.js
@@ -10,65 +10,65 @@
 
 let cat_1 = document.getElementById("cat1");
 let word = document.getElementById("collage");
-word.style.display  = 'none';
+word.style.opacity = "0.2";
 
 let cat_2 = document.getElementById("cat2");
 let word2 = document.getElementById("collage2");
-word2.style.display  = 'none';
+word2.style.opacity = "0.2";
 
 let cat_3 = document.getElementById("cat3");
 let word3 = document.getElementById("collage3");
-word3.style.display  = 'none';
+word3.style.opacity = "0.2";
 
 let cat_4 = document.getElementById("cat4");
 let word4 = document.getElementById("collage4");
-word4.style.display  = 'none';
+word4.style.opacity = "0.2";
 
 
 cat_1.addEventListener("mouseover", function(){
   // alert ("mouse over test!")
   // , (false);
-  word.style.display  = 'block';
+  word.style.opacity = "1";
 });
 
 cat_2.addEventListener("mouseover", function(){
   // alert ("mouse over test!")
   // , (false);
-  word2.style.display  = 'block';
+  word2.style.opacity = "1";
 });
 
 cat_3.addEventListener("mouseover", function(){
   // alert ("mouse over test!")
   // , (false);
-  word3.style.display  = 'block';
+  word3.style.opacity = "1";
 });
 
 cat_4.addEventListener("mouseover", function(){
   // alert ("mouse over test!")
   // , (false);
-  word4.style.display  = 'block';
+  word4.style.opacity = "1";
 });
 
 
 
 cat_1.addEventListener("mouseout", function(){
 
-  word.style.display  = 'none';
+  word.style.opacity = "0.2";
 });
 
 cat_2.addEventListener("mouseout", function(){
 
-  word2.style.display  = 'none';
+  word2.style.opacity = "0.2";
 });
 
 cat_3.addEventListener("mouseout", function(){
 
-  word3.style.display  = 'none';
+  word3.style.opacity = "0.2";
 });
 
 cat_4.addEventListener("mouseout", function(){
 
-  word4.style.display  = 'none';
+  word4.style.opacity = "0.2";
 });
 
 
