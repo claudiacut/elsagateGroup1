618bc2970507e4a640fe6d636fd011ad9014847a
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2021-01-11 15:18:45 +0100

    prove collage

--- js/alert.js
+++ js/alert.js
@@ -7,6 +7,26 @@
 // // if the user prefers reduced motion, disable parallax
 // if (motionMediaQuery.matches) rellax.destroy();
 
+let cat_1 = document.getElementById("cat1");
+
+// document.getElementById("collage").style.display = "none";
+
+cat_1.addEventListener("mouseover", function(){
+  alert ("mouse over test!")
+  , (false);
+});
+
+// document.getElementById('cat_1').addEventListener("mouseover", function(){
+//
+//   alert("WOrks");
+//
+//   collage.style.display  = 'block';
+//
+// });
+
+
+
+
 Swal.fire({
   title: 'Warning!',
   text: "This content may not be suitable for you, you have 3 seconds to change page",
