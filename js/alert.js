bbdd127637b82a96e1f901a3069bffabf6241adb
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2021-01-11 13:49:02 +0100

    inserimento collage

--- js/alert.js
+++ js/alert.js
@@ -1,3 +1,12 @@
+
+// const rellax = new Rellax('.rellax');
+//
+// // grabs the media query
+// const motionMediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+//
+// // if the user prefers reduced motion, disable parallax
+// if (motionMediaQuery.matches) rellax.destroy();
+
 Swal.fire({
   title: 'Warning!',
   text: "This content may not be suitable for you, you have 3 seconds to change page",
