a7c0773881a00c1ab3830fb2327f020a27ce72c9
claudiacut <claudiamaria.cutrupi@mail.polimi.it>
2021-01-19 17:11:29 +0100

    svg caricato prova

--- js/peppa.js
+++ js/peppa.js
@@ -1,11 +1,32 @@
-Promise.all([d3.html("./peppa.html"), d3.html(".peppa.svg")]).then(function([html, svgDocument]){
-const questionContent = d3.select(html).selectAll('body > *');
-questionContent.each(function(d){
-d3.select("#question-container").node().appendChild(this);
-});
-let svgNode = svgDocument.querySelector("svg");
-// console.log(svgNode);
-let container = document.querySelector("#visualization");
+// Promise.all([d3.html("./peppa.html"), d3.html(".peppa.svg")]).then(function([html, svgDocument]){
+// const questionContent = d3.select(html).selectAll('body > *');
+// questionContent.each(function(d){
+// // d3.select("#question-container").node().appendChild(this);
+// });
+// let svgNode = svgDocument.querySelector("svg");
+// // console.log(svgNode);
+// let container = document.querySelector("#viz");
+// // console.log(container);
+// container.appendChild(svgNode);
 // console.log(container);
-container.appendChild(svgNode);
-console.log(container);
+
+// var c = document.getElementById("myCanvas");
+// console.log(myCanvas);
+// var s =document.getElementById("#viz");
+// console.log(viz);
+
+
+
+$(function(){
+
+	$("#viz").load('peppa.svg',function(response){
+
+		$(this).addClass("svgLoaded");
+
+		if(!response){
+			// Error loading SVG!
+			// Make absolutely sure you are running this on a web server or localhost!
+		}
+
+	});
+});
