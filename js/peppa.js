7f4467afccb0f2144fb48132f01afd299ffc1fdf
claudiacut <claudiamaria.cutrupi@mail.polimi.it>
2021-01-22 18:21:50 +0100

    viz working

--- js/peppa.js
+++ js/peppa.js
@@ -23,12 +23,51 @@ $(function(){
 
 		$(this).addClass("svgLoaded");
 
-		// if(!response){
-			// Error loading SVG!
-			// Make absolutely sure you are running this on a web server or localhost!
-		// }
+    // animazioni
 
-	});
+
+
+			// $( "#sex" ).on( "click", function() {
+			//
+			// 	$( "#gore" ).css('opacity') = '0.0';
+		  //   $( "#distortion" ).css('opacity') = '0.0';
+			// 	$(this).css('opacity') = '1';
+			//
+			// };
+
+		const sex = d3.selectAll('#sex');
+    const gore = d3.selectAll('#gore');
+		const distortion = d3.selectAll('#distortion');
+
+		sex.on('click', function(){
+   gore.style('opacity',0.3);
+   distortion.style('opacity',0.3);
+  	// «this» refers to hovering element
+      d3.select(this).transition().duration(2).ease(d3.easeLinear).style('opacity',1);
+    });
+
+		gore.on('click', function(){
+   sex.style('opacity',0.3);
+   distortion.style('opacity',0.3);
+  	// «this» refers to hovering element
+      d3.select(this).transition().duration(2).ease(d3.easeLinear).style('opacity',1);
+    });
+
+		distortion.on('click', function(){
+   gore.style('opacity',0.3);
+   sex.style('opacity',0.3);
+  	// «this» refers to hovering element
+      d3.select(this).transition().duration(2).ease(d3.easeLinear).style('opacity',1);
+    });
+
+    d3.select('#sfondo').on('click', function(){
+      sex.transition().duration(1).ease(d3.easeLinear).style('opacity',1);
+			gore.transition().duration(1).ease(d3.easeLinear).style('opacity',1);
+			distortion.transition().duration(1).ease(d3.easeLinear).style('opacity',1);
+    });
+
+			});
+
 });
 
 timeout_id = setTimeout(function(){document.getElementById('dissolvenza').style.display="none"}, 5000);
