07a0401a1d797393b42d716cb125e1a3b8fb11c9
claudiacut <claudiamaria.cutrupi@mail.polimi.it>
2021-01-26 23:03:37 +0100

    storyboard spiderman

--- js/spiderman.js
+++ js/spiderman.js
@@ -3,12 +3,18 @@
 // var bt1 = document.getElementId("cn1");
 
 
-
 $(function(){
 
 	$("#vizspider").load('spiderman.svg',function(response){
 
 		$(this).addClass("svgLoaded");
+
+// 		var cn1 = document.getElementbyId("cn1");
+//
+// 		$( "cn1" ).click(function() {
+//   document.getElementById("violence").beginElement();
+// 	console.log("ciao");
+// });
 
     // animazioni
 //
@@ -19,24 +25,29 @@ $(function(){
 // });
 
 		//
-		// const sexual = d3.selectAll('#sexual');
-    // const gore = d3.selectAll('#gore');
-		// const violence = d3.selectAll('#violence');
-		// const toilet = d3.selectAll('#toilet');
-		// const pregnancy = d3.selectAll('#pregnancy');
-		// const distortion = d3.selectAll('#distortion');
-		// const originale = d3.selectAll('#originale');
-		// const fisso = d3.selectAll('#fisso');
+		const sexual = d3.selectAll('#sex');
+    const gore = d3.selectAll('#gore');
+		const violence = d3.selectAll('#violence');
+		const toilet = d3.selectAll('#toilet');
+		const pregnancy = d3.selectAll('#pregnancy');
+		const distortion = d3.selectAll('#distortion');
+		const originale = d3.selectAll('#originale');
+		const fisso = d3.selectAll('#fisso');
+
 		//
 		//
-    // cn1.on("click", function(){
-		// 	gore.style('opacity',0.3);
-	  //   pregnancy.style('opacity',0.3);
-		// 	toilet.style('opacity',0.3);
-		// 	sexual.style('opacity',0.3);
-		// 	distortion.style('opacity',0.3);
-		// 	violence.transition().duration(2).ease(d3.easeLinear).style('opacity',1);
-		//
+    d3.select("#cn1").on("click", function(){
+			gore.style('opacity',0.3);
+	    pregnancy.style('opacity',0.3);
+			toilet.style('opacity',0.3);
+			sexual.style('opacity',0.3);
+			distortion.style('opacity',0.3);
+			originale.style('opacity',0.3);
+			violence.transition().duration(2).ease(d3.easeLinear).style('opacity',1);
+		});
+
+		// sfondo.on("click", function(){
+		// 	    .style("opacity",1);
 		// });
 
 		// sexual.on('click', function(){
@@ -71,6 +82,3 @@ $(function(){
 			});
 
 });
-
-
-timeout_id = setTimeout(function(){document.getElementById('dissolvenza').style.display="none"}, 5000);
