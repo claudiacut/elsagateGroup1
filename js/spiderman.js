6f6fed3e0fac9545f661b0e39479d1019ff6becb
claudiacut <claudiamaria.cutrupi@mail.polimi.it>
2021-01-27 17:15:28 +0100

    violence&gore minecraft

--- js/spiderman.js
+++ js/spiderman.js
@@ -1,28 +1,9 @@
 
-
-// var bt1 = document.getElementId("cn1");
-
-
 $(function(){
 
 	$("#vizspider").load('spiderman.svg',function(response){
 
 		$(this).addClass("svgLoaded");
-
-// 		var cn1 = document.getElementbyId("cn1");
-//
-// 		$( "cn1" ).click(function() {
-//   document.getElementById("violence").beginElement();
-// 	console.log("ciao");
-// });
-
-    // animazioni
-//
-// var sexual = document.getElementId("sex");
-//
-//  $( "bt1" ).on( "click", function() {
-//   $( "sexual" ).style('opacity',0.3);
-// });
 
 		//
 		const sexual = d3.selectAll('#sex');
@@ -46,31 +27,7 @@ $(function(){
 			violence.transition().duration(2).ease(d3.easeLinear).style('opacity',1);
 		});
 
-		// sfondo.on("click", function(){
-		// 	    .style("opacity",1);
-		// });
 
-		// sexual.on('click', function(){
-   // gore.style('opacity',0.3);
-   // distortion.style('opacity',0.3);
-  	// // «this» refers to hovering element
-   //    d3.select(this).transition().duration(2).ease(d3.easeLinear).style('opacity',1);
-   //  });
-	 //
-		// gore.on('click', function(){
-   // sexual.style('opacity',0.3);
-   // distortion.style('opacity',0.3);
-  	// // «this» refers to hovering element
-   //    d3.select(this).transition().duration(2).ease(d3.easeLinear).style('opacity',1);
-   //  });
-	 //
-		// distortion.on('click', function(){
-   // gore.style('opacity',0.3);
-   // sexual.style('opacity',0.3);
-  	// // «this» refers to hovering element
-   //    d3.select(this).transition().duration(2).ease(d3.easeLinear).style('opacity',1);
-   //  });
-	 //
     d3.select('#sfondo').on('click', function(){
 			gore.style('opacity',1);
 			pregnancy.style('opacity',1);
